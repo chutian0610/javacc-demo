f49861ef3f022bdba2ff616c01275e03f32415ff
chutian0610 <victorchu0610@outlook.com>
2025-11-11 22:40:27 +0800

    tutorial 2 ‘ "

--- tutorial02/src/test/java/io/github/chutian0610/javacc/tutorial02/tree/CalculatorTest.java
+++ tutorial02/src/test/java/io/github/chutian0610/javacc/tutorial02/tree/CalculatorTest.java
@@ -7,8 +7,26 @@ import java.io.StringReader;
 public class CalculatorTest {
 
     @Test
-    public void testCalculate() throws ParseException {
+    public void testCalculate01() throws ParseException {
         String exp = "1+2+3";
+        CalculatorTree parser = new CalculatorTree(new StringReader(exp));
+        parser.calculate();
+        Node root = parser.jjtree.rootNode();
+        CalculatorPrintVisitor visitor = new CalculatorPrintVisitor(exp);
+        root.jjtAccept(visitor, 0);
+    }
+    @Test
+    public void testCalculate02() throws ParseException {
+        String exp = "1+(2+3)";
+        CalculatorTree parser = new CalculatorTree(new StringReader(exp));
+        parser.calculate();
+        Node root = parser.jjtree.rootNode();
+        CalculatorPrintVisitor visitor = new CalculatorPrintVisitor(exp);
+        root.jjtAccept(visitor, 0);
+    }
+    @Test
+    public void testCalculate03() throws ParseException {
+        String exp = "1+2*3";
         CalculatorTree parser = new CalculatorTree(new StringReader(exp));
         parser.calculate();
         Node root = parser.jjtree.rootNode();
