b37de7e4335e4ef55a133604f92b63630ddfb330
chutian0610 <victorchu0610@outlook.com>
2025-11-13 15:57:42 +0800

    calculator

--- tutorial02/src/test/java/io/github/chutian0610/javacc/tutorial02/tree/v2/CalculatorTest.java
+++ tutorial02/src/test/java/io/github/chutian0610/javacc/tutorial02/tree/v2/CalculatorTest.java
@@ -15,4 +15,13 @@ public class CalculatorTest {
         CalculatorPrintVisitor visitor = new CalculatorPrintVisitor(exp);
         root.jjtAccept(visitor, 0);
     }
+    @Test
+    public void testCalculate02() throws ParseException {
+        String exp = "1*(2+3)";
+        CalculatorTreeV2 parser = new CalculatorTreeV2(new StringReader(exp));
+        parser.calculate();
+        Node root = parser.jjtree.rootNode();
+        CalculatorPrintVisitor visitor = new CalculatorPrintVisitor(exp);
+        root.jjtAccept(visitor, 0);
+    }
 }
